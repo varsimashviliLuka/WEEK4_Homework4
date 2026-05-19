8c64fdb0fd11d902dec97e2febf2411616d87efe
varsimashviliLuka <varsimashvili.official@gmail.com>
2026-05-19 15:06:42 +0400

    Homework4

---
+++ Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        
+        Console.WriteLine("Luka Varsimashvili");
+        Console.WriteLine(Console.ReadLine());
+    }
+}
